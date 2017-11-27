2a342b20441aef2bcfbdc8f03d8b6529df40eccc
Sàm <haunguyen17091990@gmail.com>
2017-11-27 17:25:37 +0700

    comit

---
+++ soha/soha/Controllers/MobileController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace soha.Controllers
+{
+    public class MobileController : Controller
+    {
+        // GET: Mobile
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
