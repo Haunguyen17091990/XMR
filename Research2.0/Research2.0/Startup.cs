fe3e19087c4e98537f4d76cc7b95904546f6cc11
Sàm <haunguyen17091990@gmail.com>
2017-12-15 09:22:08 +0700

    (+) .netcore v2.0

---
+++ Research2.0/Research2.0/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ResearchWeb.Common;
+using ResearchWeb.Models;
+
+namespace Research2._0
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddApplicationInsightsTelemetry(Configuration);
+            //thuan.nguyen add
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            //end add
+            services.AddMvc();
+            //Thuan.Nguyen add
+            services.Add(new ServiceDescriptor(typeof(DBContext), new DBContext(Configuration.GetConnectionString("DefaultConnection"))));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=tinnhanh247}/{action=trangchu}/{id?}");
+            });
+            WebHelpers.Configure(app.ApplicationServices.GetRequiredService<IHttpContextAccessor>());
+        }
+    }
+}
